3f2fd6d3420acc8a07b9d00905b0109e2401529d
SchoolSubAccount <143353590+Dankari27@users.noreply.github.com>
2023-11-03 20:00:09 -0400

    added documentation

--- Tree.java
+++ Tree.java
@@ -5,10 +5,15 @@ Date: 8/3/2023
 Description: A class for a binary tree
 */
 public class Tree<T extends Comparable<T>> {
-	T data;
-	Tree<T> left;
-	Tree<T> right;
+	T data; // The data stored in the node
+	Tree<T> left; // The left child node
+	Tree<T> right; // The right child node
 
+	/**
+	 * Constructs a tree node with the specified data value.
+	 *
+	 * @param d The data to be stored in the node.
+	 */
 	public Tree(T d) {
 		data = d;
 		left = null;
